aab261987369b99b5499f6ebdd3b43b1094493d6
danis imamovic <dimamov1@umbc.edu>
2016-09-26 11:26:43 -0400

    update to personal info app

--- MainController.js
+++ MainController.js
@@ -1,10 +1,25 @@
 PersonalInfoApp.controller("MainController", ["$scope", function($scope) {
 
-	$scope.name = "";
+	$scope.Fname = "";
+	$scope.Lname = "";
 	$scope.email = "";
 	$scope.accept = "";
 	$scope.phone = ""; 
+	$scope.address = "";
+	$scope.city = "";
+	
+	$scope.names = ["USA", "Canada", "Mexico"];
+	
+	$scope.state = [ "AL", "AK", "AZ", "AR", "CA", "CO", 
+					 "CT", "DE", "FL", "GA", "HI", "ID", "IL", 
+					 "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+					 "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", 
+					 "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", 
+					 "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", 
+					 "WA", "WV", "WI", "WY"];
 
 }]);
+
+
 
  
